2fcd0c7a5eb1ff35695db775c9921ddfaf54f8af
Bruno Paulo <bruno@werules.com>
2017-01-19 18:33:00 -0200

    created /guess endpoint

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify
 import random
+import re
+from werkzeug import exceptions
 
 app = Flask(__name__)
 
@@ -10,9 +12,43 @@ dict_games = {}
 counter = 0
 
 
+def destroy_game(id):
+    dict_games.pop(int(id))
+
+
 # exports only selected fields
 def game_export(game):
-    return {'id': game['id'], 'guess': game['guess']}
+    return {'id': game['id'], 'guess': game['guess'], 'guesses': game['guesses']}
+
+
+@app.route('/guess/<id>/<letter>')
+def guess(id, letter):
+    if not id.isdigit() or len(letter) > 1 or not letter.isalpha():
+        return exceptions.BadRequest()
+    elif int(id) not in dict_games.keys():
+        return exceptions.NotFound()
+
+    game = dict_games[int(id)]
+    word = game['word']
+
+    if letter in game['guess']:
+        pass
+    elif letter in word:
+        indexes = [c.start() for c in re.finditer(letter, word)]
+        guess = game['guess']
+        for i in indexes:
+            guess = guess[:i] + letter + guess[i + 1:]
+        game['guess'] = guess
+
+        if '_' not in guess:
+            destroy_game(int(id))
+    else:
+        game['guesses'] += 1
+        if game['guesses'] >= 5:
+            destroy_game(int(id))
+            return exceptions.NotAcceptable()
+
+    return jsonify(game_export(game))
 
 
 @app.route('/start')
@@ -23,7 +59,8 @@ def start():
     game = {
         'id': counter,
         'word': word,
-        'guess': '_' * len(word)
+        'guess': '_' * len(word),
+        'guesses': 0
     }
 
     dict_games[counter] = game
