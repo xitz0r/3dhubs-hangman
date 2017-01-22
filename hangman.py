59434118a460a7edfb112776472e8609c50cbdc1
Bruno Paulo <bruno@werules.com>
2017-01-22 01:12:51 -0200

    adding wrong_letters so that users can know which letter they already guessed

--- hangman.py
+++ hangman.py
@@ -9,18 +9,20 @@ class Hangman:
         self.separator = current_app.config['SEPARATOR']
         self.guess = self.separator * len(word)
         self.wrong_guesses = 0
+        self.wrong_letters = []
         self.game_over = False
 
     def export_json(self):
         return jsonify({
             'id': self.id,
             'guess': self.guess,
-            'wrong_guesses': self.wrong_guesses
+            'wrong_guesses': self.wrong_guesses,
+            'wrong_letters': self.wrong_letters
         })
 
     def guess_letter(self, letter):
         if not self.game_over:
-            if letter in self.guess:
+            if letter in self.guess or letter in self.wrong_letters:
                 pass
             elif letter in self.word:
                 indexes = [c.start() for c in re.finditer(letter, self.word)]
@@ -28,6 +30,7 @@ class Hangman:
                     self.guess = self.guess[:i] + letter + self.guess[i + 1:]
             else:
                 self.wrong_guesses += 1
+                self.wrong_letters.append(letter)
                 if self.wrong_guesses >= 5:
                     self.game_over = True
 
